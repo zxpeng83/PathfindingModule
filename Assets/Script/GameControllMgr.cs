27cce07f9cd4d2b7b67055a089b10c0d05b97347
zxpeng83 <2118168362@qq.com>
2025-01-21 23:28:22 +0800

    other

--- Assets/Script/GameControllMgr.cs
+++ Assets/Script/GameControllMgr.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameMode
+{
+    AStar,
+}
+
 public class GameControllMgr : MonoBehaviour
 {
+    public static GameControllMgr instance;
+
+    public GameMode mode;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.mode = GameMode.AStar;
+        GraphMgr.Instance = new GraphMgr();
+        GraphMgr.Instance.scanGraph();
     }
 
     // Update is called once per frame
--- Assets/Script/GameStart.cs
+++ Assets/Script/GameStart.cs
@@ -7,9 +7,7 @@ public class GameStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GraphMgr.Instance = new GraphMgr();
-        GraphMgr.Instance.scanGraph();
-
+        
     }
 
     // Update is called once per frame
